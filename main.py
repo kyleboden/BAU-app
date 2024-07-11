70f8916e5ddf52e1d2a96c69ed1a63ab55beed91
kyleboden <124191228+kyleboden@users.noreply.github.com>
2024-07-11 17:10:47 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import setForm
 import closeForm
 
 st.set_page_config(page_title="BAU", page_icon='☀️')
-st.title(page_title + " " + page_icon)
+st.title(config.page_title + " " + config.page_icon)
 
 # --- NAVIGATION MENU ---
 selected = option_menu(
