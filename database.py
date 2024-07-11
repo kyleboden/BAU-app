46942fdf4dcfc9f527dbfbfdab2034e00ff468a2
kyleboden <124191228+kyleboden@users.noreply.github.com>
2024-07-11 16:28:45 -0600

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,29 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+creds_dict = {
+    "type": st.secrets["gcp_service_account"]["type"],
+    "project_id": st.secrets["gcp_service_account"]["project_id"],
+    "private_key_id": st.secrets["gcp_service_account"]["private_key_id"],
+    "private_key": st.secrets["gcp_service_account"]["private_key"],
+    "client_email": st.secrets["gcp_service_account"]["client_email"],
+    "client_id": st.secrets["gcp_service_account"]["client_id"],
+    "auth_uri": st.secrets["gcp_service_account"]["auth_uri"],
+    "token_uri": st.secrets["gcp_service_account"]["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["gcp_service_account"]["client_x509_cert_url"]
+}
+
+# Authenticate and create the service client
+creds = service_account.Credentials.from_service_account_info(creds_dict)
+client = authorize(creds)
+
+
 # Define the scope and credentials for Google Sheets API
 # --- Call under a function
 scope = ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-client = gspread.authorize(creds)
+#creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+#client = gspread.authorize(creds)
 
 # Open the Google Sheet
 sheet = client.open('BAU Database').sheet1
