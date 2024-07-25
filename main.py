fd937a03dcaec36238e44f7d3c26d2d1c3d0e2c7
kyleboden <124191228+kyleboden@users.noreply.github.com>
2024-07-25 15:23:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ page_icon = '☀️'
 #st.set_page_config(page_title=page_title, page_icon=page_icon)
 #st.title(page_title + " " + page_icon)
 st.set_page_config(page_title="BAU", page_icon='☀️')
-st.title(page_title + " " + page_icon)
+#st.title(page_title + " " + page_icon)
 
 import config
 import dataVisualization
