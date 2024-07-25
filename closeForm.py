58d2d1a59eb6fa387bb58fbfceaa11bdfd312428
kyleboden <124191228+kyleboden@users.noreply.github.com>
2024-07-25 16:01:04 -0600

    Update closeForm.py

--- closeForm.py
+++ closeForm.py
@@ -11,6 +11,24 @@ def update_visibility():
     else:
         st.session_state.visible = False
 
+def clear_form():
+    st.session_state.close_date = datetime.date.today()
+    st.session_state.close_time = datetime.time(14, 00)
+    st.session_state.on_time = 'Yes'
+    st.session_state.cx_email = ''
+    st.session_state.closer_name = config.closers[0] if config.closers else ''
+    st.session_state.closer_disp = config.dispositions[0] if config.dispositions else ''
+    st.session_state.lender = config.lenders[0] if config.lenders else ''
+    st.session_state.syst_size = 0
+    st.session_state.purch_pref = config.purch_prefs[0] if config.purch_prefs else ''
+    st.session_state.sold_ppw = 0
+    st.session_state.loan_amount = 0
+    st.session_state.percent_offset = 100
+    st.session_state.lock_close = False
+    st.session_state.vid_call = False
+    st.session_state.both_spouses = False
+    st.session_state.had_UB = False
+
 def close_form():
     if 'visible' not in st.session_state:
         st.session_state.visible = False
@@ -21,37 +39,39 @@ def close_form():
     
     with close_col1:
         s.questionCSS("Date you called")
-        config.close_date = st.date_input("", value=datetime.date.today()).isoformat()
+        st.session_state.close_date = st.date_input("", value=datetime.date.today())
     
     with close_col2:
         close_sub_col1, close_sub_col2 = st.columns(2)
         
         with close_sub_col1:
             s.questionCSS("Time you called")
-            config.close_time = st.time_input("", value=datetime.time(14, 00)).isoformat()
+            st.session_state.close_time = st.time_input("", value=datetime.time(14, 00))
         
         with close_sub_col2:
             s.questionCSS("Did you call on time?")
-            config.on_time = st.radio("", ['Yes', 'No'])
+            st.session_state.on_time = st.radio("", ['Yes', 'No'])
             placeholder = st.empty()
+    
     s.questionCSS("Customer's email")
-    config.cx_email = st.text_area(
+    st.session_state.cx_email = st.text_area(
         label="",
         height=100,
         placeholder="Write here"
     )
+    
     close_col3, close_col4 = st.columns(2)
     
     with close_col3:
         s.questionCSS("Closer Name")
-        config.closer_name = st.selectbox(
+        st.session_state.closer_name = st.selectbox(
             '',
             config.closers
         )
     
     with close_col4:
         s.questionCSS("Call Disposition")
-        config.closer_disp = st.selectbox(
+        st.session_state.closer_disp = st.selectbox(
             '',
             config.dispositions,
             key='closer_disp',
@@ -59,7 +79,6 @@ def close_form():
         )
     
     if st.session_state.visible:
-
         
         "---"
         
@@ -76,28 +95,28 @@ def close_form():
         
         with close_col5:
             s.questionCSS("Lender")
-            config.lender = st.selectbox(
+            st.session_state.lender = st.selectbox(
                 '',
                 config.lenders,
                 key='lender',
             )
             
             s.questionCSS("System Size")
-            config.syst_size = st.number_input(
+            st.session_state.syst_size = st.number_input(
                 '',
                 key='syst_size'
             )
         
         with close_col6:
             s.questionCSS("Loan/CASH/PPA")
-            config.purch_pref = st.selectbox(
+            st.session_state.purch_pref = st.selectbox(
                 '',
                 config.purch_prefs,
                 key='purch_pref'
             )
             
             s.questionCSS("Sold PPW")
-            config.sold_ppw = st.number_input(
+            st.session_state.sold_ppw = st.number_input(
                 '',
                 key='sold_ppw'
             )
@@ -110,7 +129,7 @@ def close_form():
             """,
             unsafe_allow_html=True
         )
-        config.loan_amount = st.number_input('')
+        st.session_state.loan_amount = st.number_input('')
         
         st.markdown(
             """
@@ -120,7 +139,7 @@ def close_form():
             """,
             unsafe_allow_html=True
         )
-        config.percent_offset = st.slider("", value=100, min_value=50, max_value=150)
+        st.session_state.percent_offset = st.slider("", value=100, min_value=50, max_value=150)
         
     close_col7, close_col8 = st.columns(2)
     
@@ -128,74 +147,84 @@ def close_form():
         st.markdown(
             "<p style='font-size: 17px; font-family: Arial, sans-serif; margin-bottom: 0px;'>Lock Close?</p>",
             unsafe_allow_html=True)
-        config.lock_close = st.checkbox('', key='lock_close')
+        st.session_state.lock_close = st.checkbox('', key='lock_close')
         
         st.markdown(
             "<p style='font-size: 17px; font-family: Arial, sans-serif; margin-bottom: 0px;'>Video Call?</p>",
             unsafe_allow_html=True)
-        config.vid_call = st.checkbox('', key='vid_call')
+        st.session_state.vid_call = st.checkbox('', key='vid_call')
     
     with close_col8:
         st.markdown(
             "<p style='font-size: 17px; font-family: Arial, sans-serif; margin-bottom: 0px;'>All decision makers?</p>",
             unsafe_allow_html=True)
-        config.both_spouses = st.checkbox('', key='both_spouses')
+        st.session_state.both_spouses = st.checkbox('', key='both_spouses')
         
         st.markdown(
             "<p style='font-size: 17px; font-family: Arial, sans-serif; margin-bottom: 0px;'>Had UB?</p>",
             unsafe_allow_html=True)
-        config.had_UB = st.checkbox('', key='had_UB')
+        st.session_state.had_UB = st.checkbox('', key='had_UB')
         
         "---"
     
     with st.form("entry_form", clear_on_submit=True):
-        st.session_state.submitted = st.form_submit_button("Submit")
+        submitted = st.form_submit_button("Submit")
         
-    if st.session_state.submitted:
+    if submitted:
         if config.closer_disp == "Closed":
             required_fields = [
-                config.close_date, config.close_time, config.on_time, config.closer_name,
-                config.closer_disp, config.cx_email, config.lender, config.syst_size,
-                config.purch_pref, config.sold_ppw, config.loan_amount, config.percent_offset
+                st.session_state.close_date, st.session_state.close_time, st.session_state.on_time, st.session_state.closer_name,
+                st.session_state.closer_disp, st.session_state.cx_email, st.session_state.lender, st.session_state.syst_size,
+                st.session_state.purch_pref, st.session_state.sold_ppw, st.session_state.loan_amount, st.session_state.percent_offset
             ]
             if not all(required_fields):
                 st.error("Please fill in all fields before submitting.")
             else:
                 new_data = [
                     config.set_date, config.set_time, config.setter_name, config.cx_state, config.cx_name,
-                    config.cx_email, config.set_comment, config.unpaid_lead, config.close_date, config.close_time,
-                    config.on_time, config.closer_name, config.closer_disp, config.lender, config.syst_size,
-                    config.purch_pref, config.close_comment, config.sold_ppw, config.loan_amount, config.percent_offset,
-                    config.lock_close, config.vid_call, config.both_spouses, config.had_UB
+                    st.session_state.cx_email, config.set_comment, config.unpaid_lead, st.session_state.close_date, st.session_state.close_time,
+                    st.session_state.on_time, st.session_state.closer_name, st.session_state.closer_disp, st.session_state.lender, st.session_state.syst_size,
+                    st.session_state.purch_pref, config.close_comment, st.session_state.sold_ppw, st.session_state.loan_amount, st.session_state.percent_offset,
+                    st.session_state.lock_close, st.session_state.vid_call, st.session_state.both_spouses, st.session_state.had_UB
                 ]
-                db.upsert_email(config.cx_email, new_data)
+                db.upsert_email(st.session_state.cx_email, new_data)
                 st.success("Customer information saved!")
+                clear_form()  # Clear form fields
                 st.experimental_rerun()
         elif config.closer_disp == "We didn't call":
             new_data = [
                 config.set_date, config.set_time, config.setter_name, config.cx_state, config.cx_name,
-                config.cx_email, config.set_comment, config.unpaid_lead, config.close_date, '', '',
-                config.closer_name, config.closer_disp, config.lender, config.syst_size, config.purch_pref,
+                st.session_state.cx_email, config.set_comment, config.unpaid_lead, st.session_state.close_date, '', '',
+                st.session_state.closer_name, st.session_state.closer_disp, st.session_state.lender, st.session_state.syst_size, st.session_state.purch_pref,
                 config.close_comment, '', '', '', '', '', '', ''
             ]
-            db.upsert_email(config.cx_email, new_data)
+            db.upsert_email(st.session_state.cx_email, new_data)
             st.success("Customer information saved!")
+            clear_form()  # Clear form fields
             st.experimental_rerun()
         else:
             required_fields = [
-                config.close_date, config.close_time, config.on_time, config.closer_name,
-                config.closer_disp, config.cx_email
+                st.session_state.close_date, st.session_state.close_time, st.session_state.on_time, st.session_state.closer_name,
+                st.session_state.closer_disp, st.session_state.cx_email
             ]
             if not all(required_fields):
                 st.error("Please fill in all fields before submitting.")
             else:
                 new_data = [
                     config.set_date, config.set_time, config.setter_name, config.cx_state, config.cx_name,
-                    config.cx_email, config.set_comment, config.unpaid_lead, config.close_date, config.close_time,
-                    config.on_time, config.closer_name, config.closer_disp, config.lender, config.purch_pref,
-                    config.close_comment, config.sold_ppw, config.loan_amount, config.percent_offset, config.lock_close,
-                    config.vid_call, config.both_spouses, config.had_UB
+                    st.session_state.cx_email, config.set_comment, config.unpaid_lead, st.session_state.close_date, st.session_state.close_time,
+                    st.session_state.on_time, st.session_state.closer_name, st.session_state.closer_disp, st.session_state.lender, st.session_state.syst_size,
+                    st.session_state.purch_pref, config.close_comment, st.session_state.sold_ppw, st.session_state.loan_amount, st.session_state.percent_offset,
+                    st.session_state.lock_close, st.session_state.vid_call, st.session_state.both_spouses, st.session_state.had_UB
                 ]
-                db.upsert_email(config.cx_email, new_data)
+                db.upsert_email(st.session_state.cx_email, new_data)
                 st.success("Customer information saved!")
+                clear_form()  # Clear form fields
                 st.experimental_rerun()
+
+# Initialize the session state if not already done
+if 'submitted' not in st.session_state:
+    st.session_state.submitted = False
+
+close_form()
+
