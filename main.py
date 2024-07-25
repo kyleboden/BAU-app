51c14b53346229ecc8b7ebd5e91372ebe652df03
kyleboden <124191228+kyleboden@users.noreply.github.com>
2024-07-25 15:23:15 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import streamlit as st
 page_title = "BAU"
 page_icon = '☀️'
 
-st.set_page_config(page_title=page_title, page_icon=page_icon)
+#st.set_page_config(page_title=page_title, page_icon=page_icon)
+#st.title(page_title + " " + page_icon)
+st.set_page_config(page_title="BAU", page_icon='☀️')
 st.title(page_title + " " + page_icon)
-
 
 import config
 import dataVisualization
