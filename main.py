3d908416e8aa5ca59cc76e54ac166724d36cdbbb
kyleboden <124191228+kyleboden@users.noreply.github.com>
2024-07-25 15:29:12 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
 from streamlit_option_menu import option_menu  # pip install streamlit-option-menu
 import streamlit as st
-page_title = "BAU"
-page_icon = '☀️'
-
-#st.set_page_config(page_title=page_title, page_icon=page_icon)
-#st.title(page_title + " " + page_icon)
 st.set_page_config(page_title="BAU", page_icon='☀️')
-#st.title(page_title + " " + page_icon)
 
 import config
 import dataVisualization
 import setForm
 import closeForm
 
-#st.set_page_config(page_title="BAU", page_icon='☀️')
 st.title(config.page_title + " " + config.page_icon)
-
-
 # --- NAVIGATION MENU ---
 selected = option_menu(
     menu_title=None,
